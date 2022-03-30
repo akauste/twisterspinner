7d80b244018cd2e7021cb6fea279cf75623c4668
Aku Kauste <aku.kauste@mediaviisi.fi>
2022-03-30 16:14:51 +0300

    Fix speach

--- src/components/ConfigureSpeach.js
+++ src/components/ConfigureSpeach.js
@@ -1,11 +1,19 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const ConfigureSpeach = (props) => {
   const {setUtterance} = props;
+  const [speach, setSpeach] = useState(true);
 
   const voiceRef = useRef();
   const synth = window.speechSynthesis;
+  if(!synth) {
+    return <p>Äänisyntetisaattori ei ole käytössä</p>;
+  }
   const voices = synth.getVoices();
+
+  const toggleSpeach = () => {
+    setSpeach(!speach);
+  }
 
   const voiceChangeHandler = (event) => {
     event.preventDefault();
@@ -13,10 +21,18 @@ const ConfigureSpeach = (props) => {
   };
 
   return (<div>
-    <label>Voice </label>
-    <select ref={voiceRef} onChange={voiceChangeHandler}>
-      { voices.map( (v,idx) => <option key={idx} value={ idx }>{ v.name } ({ v.lang })</option> )}
-    </select>
+    <div>
+      <label>Puhe syntetisaattori</label>
+      <input type="checkbox" checked={speach} onChange={toggleSpeach} />
+    </div>
+    { speach && (
+    <div className="">
+      <label>Voice </label>
+      <select ref={voiceRef} onChange={voiceChangeHandler}>
+        { voices.map( (v,idx) => <option key={idx} value={ idx }>{ v.name } ({ v.lang })</option> )}
+      </select>
+      </div>
+    )}
   </div>);
 }
 export default ConfigureSpeach;
