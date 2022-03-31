98e81a3ad2b38abe946adda7981ba208e9120df5
Aku Kauste <aku.kauste@mediaviisi.fi>
2022-03-31 13:33:20 +0300

    Make the dial rotate from previous position

--- src/components/SVGDial.js
+++ src/components/SVGDial.js
@@ -1,14 +1,39 @@
+import { useEffect, useRef, useState } from "react";
+
 const SVGDial = (props) => {
   let {position} = props;
-  let dirX, dirY; // Direction of the dial
-  if(position !== undefined) {
-    dirX = Math.sin(2*Math.PI/20*(position + 0.5));
-    dirY = -Math.cos(2*Math.PI/20*(position + 0.5));
-  }
-  else {
-    dirX = Math.sin(0);
-    dirY = -Math.cos(0);
-  }
+  // Direction of the dial
+  const dirX = Math.sin(0);
+  const dirY = -Math.cos(0);
+
+  const endDial = useRef();
+  const startAnimateRef = useRef();
+  const [dial, setDial] = useState();
+  
+  useEffect(() => {
+    console.log('useEffect', props.position);
+    if(endDial.current !== undefined) {
+      console.log('useEffect 1');
+      // We have previous value
+      const start = endDial.current;
+      endDial.current = 360/20*(position+0.5);
+      console.log('Rotate start-end:', start, endDial.current);
+      setDial(prev => ({
+        round: ( prev ? prev.round + 1 : 1),
+        now: new Date(),
+        start: start,
+        resetTime: (360-start)/360,
+        endTime: 1-(360-endDial.current)/360 
+      }));
+      if(startAnimateRef.current) {
+        startAnimateRef.current.beginElement();
+      }
+    }
+    else {
+      console.log('useEffect 3');
+      endDial.current = 0;
+    }
+  }, [position]);
 
   const spots = [];
   const color = ['red', 'green', 'blue', 'yellow', 'transparent'];
@@ -25,13 +50,15 @@ const SVGDial = (props) => {
         strokeWidth={1}
       />);
   }
-
+  
   return (<svg xmlns="http://www.w3.org/2000/svg" viewBox="-100 -100 200 200" style={{width: 200, height: 200}}>
+    <g id="background">
     <circle cx={0} cy={0} r={95} fill={'silver'}  stroke={'gray'} strokeWidth={5} />
     <circle cx={0} cy={0} r={10} fill={'gray'}  stroke={'gray'} strokeWidth={2} />
     <line x={0} y1={-98} y2={98} stroke="white" strokeWidth={0.5}></line>
     <line y={0} x1={-98} x2={98} stroke="white" strokeWidth={0.5}></line>
     { spots.map(s => s) }
+    </g>
     <g id="body-figure">
       <circle cx={0} cy={-40} r={10} fill='none' stroke="black" strokeWidth={1} />
       <line x1={0} y1={15} x2={0} y2={-30} stroke="black" strokeWidth={3} />
@@ -44,6 +71,30 @@ const SVGDial = (props) => {
       <line x1={0} y1={0} x2={dirX*90} y2={dirY*90} stroke='darkred' strokeWidth={8} />
       <circle cx={0} cy={0} r={4} fill='darkred' strokeWidth={0} />
       <circle cx={dirX*90} cy={dirY*90} r={4} fill='darkred' strokeWidth={0} />
+      { dial && <animateTransform ref={startAnimateRef} id={`reset${dial.round}`} attributeName="transform"
+                          attributeType="XML"
+                          type="rotate"
+                          from={`${dial.start} 0 0`}
+                          to="360 0 0"
+                          dur={`${dial.resetTime}s`}
+                          />}
+      { dial && <animateTransform id={`fullround${dial.round}`} attributeName="transform"
+                          attributeType="XML"
+                          type="rotate"
+                          from="0 0 0"
+                          to="360 0 0"
+                          begin={`reset${dial.round}.end`}
+                          dur="1s"
+                          repeatCount="1"/>}
+      { dial && <animateTransform id={`finish${dial.round}`} attributeName="transform"
+                          attributeType="XML"
+                          type="rotate"
+                          from="0 0 0"
+                          to={`${endDial.current} 0 0`}
+                          begin={`fullround${dial.round}.end`}
+                          dur={`${dial.endTime}s`}
+                          fill="freeze"
+                           />}
     </g>
   </svg>);
 };
